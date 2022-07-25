9322301ece4278b4b115a8d0bd288b2101211510
Rohit <rohit.singh11299@gmail.com>
2022-07-26 01:11:25 +0530

    project uploaded on git

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
+const primaryNav = document.querySelector(".ul");
+const navToggle = document.querySelector(".hamburger");
 
+navToggle.addEventListener('click', () =>{ 
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if(visibility === "false"){
+        primaryNav.setAttribute("data-visible", true);
+    }
+    else if (visibility === "true"){
+        primaryNav.setAttribute("data-visible", false);
+    }
+})
